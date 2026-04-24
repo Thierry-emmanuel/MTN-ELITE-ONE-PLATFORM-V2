c1e2ed272807a8baac48ed137ce6e56a26708d77
Abba-Max <adjiabba10@gmail.com>
2026-04-24 04:15:24 +0200

    Feature: Added pagination

---
+++ backend/src/common/dto/pagination.dto.ts
@@ -0,0 +1,22 @@
+import { IsOptional, IsPositive, Max, Min } from 'class-validator';
+import { Type } from 'class-transformer';
+import { ApiPropertyOptional } from '@nestjs/swagger';
+
+export class PaginationDto {
+  @ApiPropertyOptional({ default: 1 })
+  @IsOptional()
+  @Type(() => Number)
+  @IsPositive()
+  page?: number = 1;
+
+  @ApiPropertyOptional({ default: 10 })
+  @IsOptional()
+  @Type(() => Number)
+  @Min(1)
+  @Max(100)
+  limit?: number = 10;
+
+  get skip(): number {
+    return ((this.page ?? 1) - 1) * (this.limit ?? 10);
+  }
+}
